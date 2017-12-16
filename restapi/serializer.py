01cd5117262c467c1b3a6e5e2c3aa0996ff0b184
Shimbo Takaaki <ta7uwtaka@gmail.com>
2017-12-16 11:14:41 +0900

    remove method

--- restapi/serializer.py
+++ restapi/serializer.py
@@ -12,7 +12,3 @@ class ManyItemsSerializer(serializers.Serializer):
     """ All 'Item' Model serialize. """
     name = serializers.CharField()
     items = ItemSerializer(many=True, allow_null=True, default=Item.objects.all())
-
-    def create(self, validated_data):
-        """ Create and return a new 'Item' instance, given the validated data. """
-        return Item.objects.create(**validated_data)
